301b2b1486c2faf56318ef1c705186a28f8a1307
Daniel Tsai <danieltsai.developer@gmail.com>
2017-08-09 22:18:01 -0700

    Add buttons on root route

--- src/containers/Root/index.jsx
+++ src/containers/Root/index.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Row from 'react-bootstrap/lib/Row'
+import Col from 'react-bootstrap/lib/Col'
+import Button from 'react-bootstrap/lib/Button'
 
 class Root extends React.Component {
     render() {
         return (
-            <div>Root Component</div>
+            <Row>
+                <Col xs={12} sm={6} smOffset={3}>
+                    <Button bsStyle="primary" block>View Films</Button>
+                    <Button bsStyle="primary" block>View People</Button>
+                    <Button bsStyle="primary" block>View Locations</Button>
+                    <Button bsStyle="primary" block>View Species</Button>
+                    <Button bsStyle="primary" block>View Vehicles</Button>
+                </Col>
+            </Row>
         )
     }
 }
