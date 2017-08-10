8ed2a1f300a42913399a925d21bc4a014247a2e7
Daniel Tsai <danieltsai.developer@gmail.com>
2017-08-09 21:29:07 -0700

    Install dependencies for project

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # studio-ghibli-ui
 Web App for Studio Ghibli API https://ghibliapi.herokuapp.com/
+
+## Setting up the project
+First install all npm dependencies
+```
+npm install
+```
+
+## Running on a local server
+Run webpack to start local server
+```
+npm start
+```
+Point browser to [localhost:3000](localhost:3000)
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+var webpack = require('webpack')
+var path = require('path')
+
+var PUBLIC_DIR = path.resolve(__dirname, 'public')
+var BUILD_DIR = path.resolve(__dirname, 'public/dist')
+var APP_DIR = path.resolve(__dirname, 'src')
+var ASSET_DIR = path.resolve(__dirname, 'src/assets')
+
+var PROD = process.env.NODE_ENV || 'production'
+
+module.exports = {
+    entry: APP_DIR + '/index.jsx',
+    output: {
+        path: BUILD_DIR,
+        publicPath: '/dist/',
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: [ '.js', '.jsx', '.json' ]
+    },
+    module: {
+        loaders : [
+            {
+                test: /\.jsx?/,
+                include : APP_DIR,
+                loader : 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                loaders: [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test: /\.scss$/,
+                loaders: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            },
+            {
+                test: /\.(jpe?g|png)$/i,
+                include: ASSET_DIR,
+                loader: 'file-loader'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify(PROD)
+            }
+        })
+    ],
+    devServer: {
+        port: 3000,
+        historyApiFallback: true,
+        inline: true,
+        contentBase: PUBLIC_DIR
+    }
+}
